a347ad064572c1a2fc6025e31e6950338d43cb50
Ritch Melton <skyguy94@gmail.com>
2015-01-16 09:35:21 -0600

    Import from codeplex

---
+++ DynamicWrapper/DynamicWrapper.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Threading;
+
+namespace HouseOfBilz
+{
+    public static class ObjectExtensions
+    {
+        public static T As<T>(this object realObject) where T : class
+        {
+            if(realObject is T)
+                return realObject as T;
+
+            return DynamicWrapper.DynamicWrapper.CreateWrapper<T>(realObject);
+        }
+
+        public static T AsReal<T>(this object wrapper) where T : class
+        {
+            if (wrapper is T)
+                return wrapper as T;
+
+            if (wrapper is DynamicWrapper.DynamicWrapper.DynamicWrapperBase)
+                return (T)(wrapper as DynamicWrapper.DynamicWrapper.DynamicWrapperBase).RealObject;
+
+            return null;
+        }
+    }
+}
+
+namespace HouseOfBilz.DynamicWrapper
+{
+    public class DynamicWrapper
+    {
+        public class DynamicWrapperBase
+        {
+            internal protected object RealObject;
+        }
+
+        private static readonly ModuleBuilder _moduleBuilder;
+
+        static DynamicWrapper()
+        {
+            var assembly = Thread.GetDomain().DefineDynamicAssembly(new AssemblyName("DynamicWrapper"), AssemblyBuilderAccess.Run);
+            _moduleBuilder = assembly.DefineDynamicModule("DynamicWrapperModule", false);
+        }
+
+        private static readonly WrapperDictionary _wrapperDictionary = new WrapperDictionary();
+
+        public static Type GetWrapper(Type interfaceType, Type realObjectType)
+        {
+            Type wrapperType = _wrapperDictionary.GetType(interfaceType, realObjectType);
+            
+            if(wrapperType == null)
+            {
+                wrapperType = GenerateWrapperType(interfaceType, realObjectType);
+                _wrapperDictionary.SetType(interfaceType, realObjectType, wrapperType);
+            }
+
+            return wrapperType;
+        }
+
+        private static Type GenerateWrapperType(Type InterfaceType, Type RealObjectType)
+        {
+            var wrapperName = string.Format("{0}_{1}_Wrapper", InterfaceType.Name, RealObjectType.Name);
+
+            TypeBuilder wrapperBuilder = _moduleBuilder.DefineType(
+                wrapperName,
+                TypeAttributes.NotPublic | TypeAttributes.Sealed,
+                typeof(DynamicWrapperBase),
+                new[] { InterfaceType });
+
+            var wrapperMethod = new WrapperMethodBuilder(RealObjectType, wrapperBuilder);
+
+            foreach (MethodInfo method in InterfaceType.AllMethods())
+            {
+                wrapperMethod.Generate(method);
+            }
+
+            return wrapperBuilder.CreateType();
+        }
+
+        public static T CreateWrapper<T>(object realObject) where T : class
+        {
+            var dynamicType = GetWrapper(typeof(T), realObject.GetType());
+            var dynamicWrapper = (DynamicWrapperBase)Activator.CreateInstance(dynamicType);
+            
+            dynamicWrapper.RealObject = realObject;
+
+            return dynamicWrapper as T;
+        }
+    }
+
+    internal class WrapperMethodBuilder
+    {
+        private readonly Type _realObjectType;
+        private readonly TypeBuilder _wrapperBuilder;
+
+        public WrapperMethodBuilder(Type realObjectType, TypeBuilder proxyBuilder)
+        {
+            _realObjectType = realObjectType;
+            _wrapperBuilder = proxyBuilder;
+        }
+
+        public void Generate(MethodInfo newMethod)
+        {
+            if (newMethod.IsGenericMethod)
+                newMethod = newMethod.GetGenericMethodDefinition();
+
+            FieldInfo srcField = typeof(DynamicWrapper.DynamicWrapperBase).GetField("RealObject", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            var parameters = newMethod.GetParameters();
+            var parameterTypes = parameters.Select(parameter => parameter.ParameterType).ToArray();
+
+            var methodBuilder = _wrapperBuilder.DefineMethod(
+                newMethod.Name,
+                MethodAttributes.Public | MethodAttributes.Virtual, 
+                newMethod.ReturnType, 
+                parameterTypes);
+
+            if (newMethod.IsGenericMethod)
+            {
+                methodBuilder.DefineGenericParameters(
+                    newMethod.GetGenericArguments().Select(arg => arg.Name).ToArray());
+            }
+
+            ILGenerator ilGenerator = methodBuilder.GetILGenerator();
+
+            LoadRealObject(ilGenerator, srcField);
+            PushParameters(parameters, ilGenerator);
+            ExecuteMethod(newMethod, parameterTypes, ilGenerator);
+            Return(ilGenerator);
+        }
+
+        private static void Return(ILGenerator ilGenerator)
+        {
+            ilGenerator.Emit(OpCodes.Ret);
+        }
+
+        private void ExecuteMethod(MethodBase newMethod, Type[] parameterTypes, ILGenerator ilGenerator)
+        {
+            MethodInfo srcMethod = GetMethod(newMethod, parameterTypes);
+
+            if (srcMethod == null) throw new MissingMethodException("Unable to find method " + newMethod.Name + " in " + _realObjectType.FullName);
+
+            ilGenerator.Emit(OpCodes.Call, srcMethod);
+        }
+
+        private MethodInfo GetMethod(MethodBase realMethod, Type[] parameterTypes)
+        {
+            if (realMethod.IsGenericMethod)
+                return _realObjectType.GetGenericMethod(realMethod.Name, parameterTypes);
+
+            return _realObjectType.GetMethod(realMethod.Name, parameterTypes);
+        }
+
+        private static void PushParameters(ICollection<ParameterInfo> parameters, ILGenerator ilGenerator)
+        {
+            for (int i = 1; i < parameters.Count + 1; i++)
+                ilGenerator.Emit(OpCodes.Ldarg, i);
+        }
+
+        private static void LoadRealObject(ILGenerator ilGenerator, FieldInfo srcField)
+        {
+            ilGenerator.Emit(OpCodes.Ldarg_0);
+            ilGenerator.Emit(OpCodes.Ldfld, srcField);
+        }
+    }
+
+    internal class WrapperDictionary
+    {
+        private readonly Dictionary<string, Type> _wrapperTypes = new Dictionary<string, Type>();
+
+        private static string GenerateKey(Type interfaceType, Type realObjectType)
+        {
+            return interfaceType.Name + "->" + realObjectType.Name;
+        }
+
+        public Type GetType(Type interfaceType, Type realObjectType)
+        {
+            string key = GenerateKey(interfaceType, realObjectType);
+
+            if (_wrapperTypes.ContainsKey(key))
+                return _wrapperTypes[key];
+
+            return null;
+        }
+
+        public void SetType(Type interfaceType, Type realObjectType, Type wrapperType)
+        {
+            string key = GenerateKey(interfaceType, realObjectType);
+
+            if (_wrapperTypes.ContainsKey(key))
+                _wrapperTypes[key] = wrapperType;
+            else
+                _wrapperTypes.Add(key, wrapperType);
+        }
+
+    }
+
+    public static class TypeExtensions
+    {
+        public static MethodInfo GetGenericMethod(this Type type, string name, params Type[] parameterTypes)
+        {
+            var methods = type.GetMethods().Where(method => method.Name == name);
+            
+            foreach (var method in methods)
+            {
+                if(method.HasParameters(parameterTypes))
+                    return method;
+            }
+
+            return null;
+        }
+
+        public static bool HasParameters(this MethodInfo method, params Type[] parameterTypes)
+        {
+            var methodParameters = method.GetParameters().Select(parameter => parameter.ParameterType).ToArray();
+
+            if(methodParameters.Length != parameterTypes.Length)
+                return false;
+
+            for(int i = 0; i < methodParameters.Length; i++)
+                if(methodParameters[i].ToString() != parameterTypes[i].ToString())
+                    return false;
+
+            return true;
+        }
+
+        public static IEnumerable<Type> AllInterfaces(this Type target)
+        {
+            foreach (var IF in target.GetInterfaces())
+            {
+                yield return IF;
+                foreach (var childIF in IF.AllInterfaces())
+                {
+                    yield return childIF;
+                }
+            }
+        }
+
+        public static IEnumerable<MethodInfo> AllMethods(this Type target)
+        {
+            var allTypes = target.AllInterfaces().ToList();
+            allTypes.Add(target);
+
+            return from type in allTypes
+                   from method in type.GetMethods()
+                   select method;
+        }
+    }
+}
---
+++ DynamicWrapperTests/DynamicWrapperTests.cs
@@ -0,0 +1,351 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using HouseOfBilz;
+
+namespace HouseOfBilz
+{
+    [TestFixture]
+    public class DynamicWrapperTests
+    {
+        public class TestEventArgs : EventArgs
+        {
+            public string Message { get; set; }
+        }
+
+        public interface ITester
+        {
+            string Repsond(string message);
+            int Return5();
+            void VoidMethod();
+            void Add6ToParam(ref int param);
+            void SetOutParamTo7(out int param);
+            int Property { get; set; }
+
+            event EventHandler<TestEventArgs> MyEvent;
+        }
+
+        public interface ITester2
+        {
+            string Repsond(string message);
+        }
+
+        public interface IGenericTester
+        {
+            bool IsTypeOf<T>();
+            bool TypesAreSame<OneT, TwoT>();
+            T GenericReturn<T>();
+            T GenericParameter<T>(T value);
+            bool GenericCompare<OneT, TwoT>(OneT one, TwoT two);
+            IEnumerable<T> Return3<T>(Func<int, T> generate);
+        }
+
+        public class MyTester
+        {
+            public string Repsond(string message) { return message + " response"; }
+            public int Return5() { return 5; }
+            public void VoidMethod() { throw new Exception("VoidMethod"); }
+            public void Add6ToParam(ref int param) { param = param + 6; }
+            public void SetOutParamTo7(out int param) { param = 7; }
+            public int Property { get; set; }
+
+            public bool IsTypeOf<T>() { return typeof (T) == GetType(); }
+            public bool TypesAreSame<OneT, TwoT>() { return typeof (OneT) == typeof (TwoT); }
+            public T GenericReturn<T>() {return (T)Activator.CreateInstance(typeof (T)); }
+            public T GenericParameter<T>(T value) {return value;}
+            public bool GenericCompare<OneT, TwoT>(OneT one, TwoT two) {return one.Equals(two);}
+            public IEnumerable<T> Return3<T>(Func<int, T> generate)
+            {
+                for (int i = 0; i < 3; i++)
+                    yield return generate(i);
+            }
+
+            public event EventHandler<TestEventArgs> MyEvent;
+
+            public void FireEvent(string message)
+            {
+                var handlers = MyEvent;
+                if(handlers != null)
+                    handlers(this, new TestEventArgs{Message = message});
+            }
+        }
+
+        public interface IGenericInterface<T, F>
+        {
+            T GetT();
+            F GetF();
+        }
+
+        public class GenericClass<T, F>
+        {
+            public T TVal = default(T);
+            public F FVal = default(F);
+
+            public T GetT()
+            {
+                return TVal;
+            }
+
+            public F GetF()
+            {
+                return FVal;
+            }
+        }
+
+        private MyTester _realObject;
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            _realObject = new MyTester();
+        }
+
+        [Test]
+        public void Calling_Wrapper_Proxies_To_RealObject()
+        {
+            var wrappedObject = _realObject.As<ITester>();
+
+            Assert.That(wrappedObject.Repsond("Foo"), Is.EqualTo("Foo response"));
+        }
+
+        [Test]
+        public void Casting_More_Than_Once_Does_Not_Fail()
+        {
+            var wrappedObject1 = _realObject.As<ITester>();
+            var wrappedObject2 = _realObject.As<ITester>();
+
+            Assert.That(wrappedObject1.Repsond("Foo"), Is.EqualTo("Foo response"));
+            Assert.That(wrappedObject2.Repsond("Foo"), Is.EqualTo("Foo response")); 
+        }
+
+        [Test]
+        public void Two_Interface_Wrappers_For_The_Same_Class()
+        {
+            var wrappedObject1 = _realObject.As<ITester>();
+            var wrappedObject2 = _realObject.As<ITester2>();
+
+            Assert.That(wrappedObject1.Repsond("Foo"), Is.EqualTo(wrappedObject2.Repsond("Foo")));
+        }
+
+        [Test]
+        public void Parameterless_Methods_Wrap_Propperly()
+        {
+            var wrappedObject = _realObject.As<ITester>();
+
+            Assert.That(wrappedObject.Return5(), Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Reference_Parameters_Wrap_Propperly()
+        {
+            var wrappedObject = _realObject.As<ITester>();
+
+            int foo = 5;
+            wrappedObject.Add6ToParam(ref foo);
+
+            Assert.That(foo, Is.EqualTo(11));
+        }
+
+        [Test]
+        public void Out_Parameters_Wrap_Propperly()
+        {
+            var wrappedObject = _realObject.As<ITester>();
+
+            int foo;
+            wrappedObject.SetOutParamTo7(out foo);
+
+            Assert.That(foo, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void Void_Methods_Wrap_Propperly()
+        {
+            var wrappedObject = _realObject.As<ITester>();
+
+            Exception exception = null;
+            try { wrappedObject.VoidMethod(); }
+            catch(Exception e) { exception = e; }
+
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception.Message, Is.EqualTo("VoidMethod"));
+        }
+
+        [Test]
+        public void Set_Properties_Wrap_Propperly()
+        {
+            var wrappedObject = _realObject.As<ITester>();
+
+            wrappedObject.Property = 55;
+            Assert.That(_realObject.Property, Is.EqualTo(55));
+        }
+
+        [Test]
+        public void Get_Properties_Wrap_Propperly()
+        {
+            var wrappedObject = _realObject.As<ITester>();
+
+            _realObject.Property = 66;
+            Assert.That(wrappedObject.Property, Is.EqualTo(66));
+        }
+
+        [Test]
+        public void Generic_Methods_With_One_Argument_Generates_Properly()
+        {
+            var wrappedObject = _realObject.As<IGenericTester>();
+
+            Assert.That(wrappedObject.IsTypeOf<string>(), Is.False);
+        }
+
+        [Test]
+        public void Generic_Methods_With_Multiple_Arguments_Generates_Properly()
+        {
+            var wrappedObject = _realObject.As<IGenericTester>();
+
+            Assert.That(wrappedObject.TypesAreSame<string, string>(), Is.True);
+        }
+
+        [Test]
+        public void Generic_Return_Value_Generates_Properly()
+        {
+            var wrappedObject = _realObject.As<IGenericTester>();
+
+            Assert.That(wrappedObject.GenericReturn<int>(), Is.EqualTo(0));
+            Assert.That(wrappedObject.GenericReturn<DateTime>(), Is.EqualTo(DateTime.MinValue));
+        }
+
+        [Test]
+        public void Generic_Parameter_Generates_Properly()
+        {
+            var wrappedObject = _realObject.As<IGenericTester>();
+
+            Assert.That(wrappedObject.GenericParameter("FOO"), Is.EqualTo("FOO"));
+        }
+
+        [Test]
+        public void Multiple_Generic_Parameters_Generates_Properly()
+        {
+            var wrappedObject = _realObject.As<IGenericTester>();
+
+            Assert.That(wrappedObject.GenericCompare("FOO", "FOO"));
+            Assert.That(wrappedObject.GenericCompare("FOO", "BAR"), Is.False);
+        }
+
+        [Test]
+        public void Complex_Generic_Method_Generates_Properly()
+        {
+            var wrappedObject = _realObject.As<IGenericTester>();
+
+            var values = wrappedObject.Return3(i => i.ToString()).ToList();
+
+            Assert.That(values.Count, Is.EqualTo(3));
+            Assert.That(values[0], Is.EqualTo("0"));
+            Assert.That(values[1], Is.EqualTo("1"));
+            Assert.That(values[2], Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void Generic_Interface_Generates_Properly()
+        {
+            var realObject = new GenericClass<string, int> {TVal = "FOO", FVal = 99};
+            var wrappedObject = realObject.As<IGenericInterface<string, int>>();
+
+            Assert.That(wrappedObject.GetT(), Is.EqualTo("FOO"));
+            Assert.That(wrappedObject.GetF(), Is.EqualTo(99));
+        }
+
+        [Test]
+        public void Events_Wrap_Propperly()
+        {
+            var wrappedObject = _realObject.As<ITester>();
+
+            object eventSender = null;
+            string eventMessage = string.Empty;
+
+            wrappedObject.MyEvent += (sender, args) =>
+                                         {
+                                             eventSender = sender;
+                                             eventMessage = args.Message;
+                                         };
+            
+            _realObject.FireEvent("FOO");
+
+            Assert.That(eventSender, Is.EqualTo(_realObject));
+            Assert.That(eventMessage, Is.EqualTo(eventMessage));
+        }
+
+        [Test]
+        public void GetRealObjectBack()
+        {
+            var wrappedObject = _realObject.As<ITester>();
+
+            Assert.That(wrappedObject.AsReal<MyTester>(), Is.EqualTo(_realObject));
+        }
+
+        [Test]
+        public void GetRealObject_When_Type_Implements_Interface()
+        {
+            IList list = new List<string>();
+            var list2 = list.AsReal<List<string>>();
+
+            Assert.That(list, Is.EqualTo(list2));
+        }
+
+        [Test]
+        public void GetRealObject_When_Wrong_Type_Returns_Null()
+        {
+            IList list = new List<string>();
+
+            Assert.That(list.AsReal<MyTester>(), Is.Null);
+        }
+
+        public interface INotWrappable
+        {
+            void NotWrappable();
+        }
+
+        [Test, ExpectedException(typeof(MissingMethodException))]
+        public void Bad_Mapping_Throws_Execption()
+        {
+            var wrappedObject = _realObject.As<INotWrappable>();
+        }
+
+        public interface IDerived : IDisposable
+        {
+            string Foo();
+        }
+
+        public class MultiIF
+        {
+            private string _message = "Bar";
+            public void Dispose() { _message = "Disposed"; }
+            public string Foo() { return _message; }
+        }
+
+        [Test]
+        public void Derived_Interfaces_Generate_Properly()
+        {
+            var realObject = new MultiIF();
+            var wrappedObject = realObject.As<IDerived>();
+
+            Assert.That(wrappedObject.Foo(), Is.EqualTo("Bar"));
+
+            wrappedObject.Dispose();
+
+            Assert.That(wrappedObject.Foo(), Is.EqualTo("Disposed"));
+        }
+
+        public interface IFoo {}
+        public class Foo : IFoo {}
+
+        [Test]
+        public void When_Class_Explicitly_Implements_Interface_Do_Not_Wrap()
+        {
+            var realObject = new Foo();
+            var foo = realObject.As<IFoo>();
+
+            Assert.That(realObject, Is.EqualTo(foo));
+        }
+    }
+}
