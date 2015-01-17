00fb1e52b055ca51be38c169a3226ccfc9b36b39
Ritch Melton <skyguy94@gmail.com>
2015-01-16 23:54:57 -0600

    Add sigil. Keep old code as temporary sigil workaround

--- DynamicWrapper/DynamicWrapper.cs
+++ DynamicWrapper/DynamicWrapper.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.InteropServices;
 using System.Threading;
 using Sigil.NonGeneric;
 
@@ -27,7 +28,7 @@ namespace DynamicWrapper
     public static T CreateWrapper<T>(object realObject) where T : class
     {
       var dynamicType = GetWrapper(typeof(T), realObject.GetType());
-      var dynamicWrapper = (DynamicWrapperBase)Activator.CreateInstance(dynamicType);
+      var dynamicWrapper = (DynamicWrapperBase) Activator.CreateInstance(dynamicType);
 
       dynamicWrapper.RealObject = realObject;
 
@@ -69,18 +70,54 @@ namespace DynamicWrapper
     {
       if (methodDefinition.IsGenericMethod)
       {
-        methodDefinition = methodDefinition.GetGenericMethodDefinition();
+        EmitGenericMethod(methodDefinition.GetGenericMethodDefinition(), objectType, typeBuilder);
       }
+      else
+      {
+        EmitMethod(methodDefinition, objectType, typeBuilder);
+      }
+    }
 
-      var sourceField = typeof(DynamicWrapperBase)
-        .GetField("RealObject", BindingFlags.Instance | BindingFlags.NonPublic);
 
+    private static void EmitGenericMethod(MethodInfo methodDefinition, Type objectType, TypeBuilder typeBuilder)
+    {
       var parameters = methodDefinition.GetParameters().ToList();
       var parameterTypes = parameters.Select(parameter => parameter.ParameterType).ToArray();
 
-      var method = methodDefinition.IsGenericMethodDefinition
-        ? objectType.GetGenericMethod(methodDefinition.Name, parameterTypes)
-        : objectType.GetMethod(methodDefinition.Name);
+      var method = objectType.GetGenericMethod(methodDefinition.Name, parameterTypes);
+      if (method == null) throw new MissingMethodException();
+
+      var methodBuilder = typeBuilder.DefineMethod(
+        methodDefinition.Name,
+        MethodAttributes.Public | MethodAttributes.Virtual,
+        methodDefinition.ReturnType,
+        parameterTypes);
+
+      methodBuilder.DefineGenericParameters(methodDefinition.GetGenericArguments().Select(arg => arg.Name).ToArray());
+
+      var ilGenerator = methodBuilder.GetILGenerator();
+
+      ilGenerator.Emit(OpCodes.Ldarg_0);
+      var sourceField = typeof(DynamicWrapperBase).GetField("RealObject", BindingFlags.Instance | BindingFlags.NonPublic);
+      if (sourceField == null) throw new InvalidOperationException();
+
+      ilGenerator.Emit(OpCodes.Ldfld, sourceField);
+
+      for (int i = 1; i < parameters.Count + 1; i++)
+      {
+        ilGenerator.Emit(OpCodes.Ldarg, i);
+      }
+
+      ilGenerator.Emit(OpCodes.Call, method);
+      ilGenerator.Emit(OpCodes.Ret);
+    }
+
+    private static void EmitMethod(MethodInfo methodDefinition, Type objectType, TypeBuilder typeBuilder)
+    {
+      var parameters = methodDefinition.GetParameters().ToList();
+      var parameterTypes = parameters.Select(parameter => parameter.ParameterType).ToArray();
+
+      var method = objectType.GetMethod(methodDefinition.Name);
       if (method == null) throw new MissingMethodException();
 
       var methodWrapper = Emit.BuildInstanceMethod(methodDefinition.ReturnType,
@@ -90,6 +127,7 @@ namespace DynamicWrapper
         MethodAttributes.Public | MethodAttributes.Virtual,
         doVerify: false);
 
+      var sourceField = typeof(DynamicWrapperBase).GetField("RealObject", BindingFlags.Instance | BindingFlags.NonPublic);
       methodWrapper.LoadArgument(0);
       methodWrapper.LoadField(sourceField);
       for (ushort i = 1; i < parameters.Count + 1; i++)
